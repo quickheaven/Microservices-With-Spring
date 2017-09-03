48e895931a13b02b28082a4ab6043529f76f9706
acristobal <quickheaven@gmail.com>
2017-09-02 23:02:12 -0400

    Spring Cloud Configuration - Centralized, Versioned Configuration - Lab 3 - Config Server and Client

---
+++ lab-3-client/src/main/java/demo/LuckyWordController.java
@@ -0,0 +1,23 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LuckyWordController {
+	
+	@Value("${lucky-word}")
+	String luckyWord;
+	
+	@RequestMapping("/lucky-word")
+	public String showLuckyWord() {
+		return "The luck word is: " + luckyWord;
+	}
+
+}
+
+
+
+
