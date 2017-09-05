e5308947ce6faab52997e7c917b2dc8dfb05f2b8
acristobal <quickheaven@gmail.com>
2017-09-04 22:01:09 -0400

    Lab 5 - Using Ribbon Clients. Part 1, Run Config Server, Eureka, and the word servers

---
+++ lab-5-sentence/src/main/java/demo/SentenceController.java
@@ -0,0 +1,60 @@
+package demo;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class SentenceController {
+
+	@Autowired 
+	private DiscoveryClient client;
+	
+	@RequestMapping("/sentence")
+	public @ResponseBody String getSentence() {
+	  return 
+		"<h3>Some Sentences</h3><br/>" +	  
+		buildSentence() + "<br/><br/>" +
+		buildSentence() + "<br/><br/>" +
+		buildSentence() + "<br/><br/>" +
+		buildSentence() + "<br/><br/>" +
+		buildSentence() + "<br/><br/>"
+		;
+	}
+	
+	public String buildSentence() {
+		String sentence = "There was a problem assembling the sentence!";
+		try{
+			sentence =  
+				String.format("%s %s %s %s %s.",
+					getWord("subject"),
+					getWord("verb"),
+					getWord("article"),
+					getWord("adjective"),
+					getWord("noun") );			
+		} catch ( Exception e ) {
+			System.err.println(e);
+		}
+		return sentence;
+	}
+	
+	public String getWord(String service) {
+		//List<String> services = client.getServices();
+        List<ServiceInstance> list = client.getInstances(service);
+        if (list != null && list.size() > 0 ) {
+      	URI uri = list.get(0).getUri();
+	      	if (uri !=null ) {
+	      		return (new RestTemplate()).getForObject(uri,String.class);
+	      	}
+        }
+        return null;
+	}
+	
+}
