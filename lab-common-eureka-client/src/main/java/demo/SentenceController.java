ed93db2d15b13494f1c80a54f63d2493e9f23fd8
acristobal <quickheaven@gmail.com>
2017-09-04 21:06:45 -0400

    Lab 4 - removed duplicate code. Implement a single word server which selects different words based on Spring profile.

---
+++ lab-common-eureka-client/src/main/java/demo/SentenceController.java
@@ -0,0 +1,36 @@
+package demo;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SentenceController {
+
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping("/sentence")
+	public @ResponseBody String getSentence() {
+		return getWord("lab-4-subject") + " " + getWord("lab-4-verb") + " " + getWord("lab-4-article") + " " + getWord("lab-4-adjective") + " " + getWord("lab-4-noun") + ".";
+	}
+
+	public String getWord(String service) {
+		List<ServiceInstance> list = client.getInstances(service);
+		if (list != null && list.size() > 0) {
+			URI uri = list.get(0).getUri();
+			if (uri != null) {
+				return (new RestTemplate()).getForObject(uri, String.class);
+			}
+		}
+		return null;
+	}
+	
+}
